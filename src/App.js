b297c00a347d64e46afdd7e6a302427c9e5a2047
L-Lisa <lisaojeland@gmail.com>
2020-04-03 13:17:55 +0200

    basics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import React from 'react'
+import {Card} from "./Card"
+
+
 
 export const App = () => {
+
+  const cardArray =[
+
+    {  
+      className: "Unicorn",
+      title: "Unicorn",
+      emoji:" 🦄",
+      txt:"Magic pony",
+      btnTxt: "Buy one here",
+      primarycolor:"pink",
+      secondarycolor:"lightblue"
+    },
+  {  
+    className: "lemon",
+    title: "Lemon",
+    emoji:" 🍋",
+    txt:"This is a really tasty fruit",
+    btnTxt: "order now",
+    primarycolor:"lightgreen",
+    secondarycolor:"lightblue"
+  },
+  {
+    className: "rocket",
+    title: "Rocket",
+    emoji:" 🚀",  
+    txt:"Join us into outer space",
+    btnTxt: "order now",
+    primarycolor:"grey",
+    secondarycolor:"lightblue"
+  },
+  {
+    className:"doggy",
+    title:"Doggy",
+    emoji:" 🐶",
+    txt:"This is a really tasty fruit",
+    btnTxt:"adopt me",
+    primarycolor:"lightgreen",
+    secondarycolor:"mustard"
+  }
+
+
+  ]
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="cards-wrapper">
+      <h1>Pretty Cards</h1>
+      <section>
+        {cardArray.map(cardItem => (
+        <Card {...cardItem}/>
+      ))}
+      </section>  
     </div>
   )
 }
---
+++ src/Card.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react"
+
+
+export const Card = props => {
+    const [showComponent, setShowComponent]= useState(false)
+
+
+const onClick = () => {
+    setShowComponent(true)
+}
+ return (
+
+    <div>
+        {!showComponent ? (
+            <div className="main-card" style={{ backgroundColor: props.primarycolor }} >
+                <div style={{ backgroundColor: props.secondarycolor }} className="header">
+                    <span className="title-txt">{props.title}</span>
+                    <span className="emoji" aria-hidden="true">{props.emoji}</span>
+                </div>
+                <section  className="card-body">
+                    {props.txt}
+                    <button onClick = {onClick}>
+                        <p className="btntxt">{props.btnTxt} </p>
+                    </button>
+                </section>
+            </div>
+
+        ): (
+        <section className="component-wrapper">
+            <div className="component">
+                <section style={{ backgroundColor: props.secondarycolor }} className="component-header">
+                   <h1> {props.title}</h1>
+                </section>
+                <div style={{ backgroundColor: props.primarycolor }} className="component-emoji">
+                    <span>{props.emoji}</span>
+                </div>
+            </div>
+        </section>
+        )}
+    </div>
+    )
+}
